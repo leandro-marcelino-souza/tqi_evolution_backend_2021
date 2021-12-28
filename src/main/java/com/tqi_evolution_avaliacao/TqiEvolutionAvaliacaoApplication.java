f441c0eeb3fda502bb9e9ab00e3abb5bb781eae2
Leandro Marcelino <leandro.souza.marcelino@gmail.com>
2021-12-27 23:59:34 -0300

    Testando o Salvamento e consulta dos  Clientes

--- src/main/java/com/tqi_evolution_avaliacao/TqiEvolutionAvaliacaoApplication.java
+++ src/main/java/com/tqi_evolution_avaliacao/TqiEvolutionAvaliacaoApplication.java
@@ -1,13 +1,32 @@
 package com.tqi_evolution_avaliacao;
 
+import com.tqi_evolution_avaliacao.entity.Address;
+import com.tqi_evolution_avaliacao.entity.Client;
+import com.tqi_evolution_avaliacao.repositories.AddressRepository;
+import com.tqi_evolution_avaliacao.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class TqiEvolutionAvaliacaoApplication {
+	@Autowired
+	ClientRepository clientRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TqiEvolutionAvaliacaoApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			List<Client> clients = clientRepository.findAll();
+			clients.forEach(client -> System.out.println(client.getEmail()));
+		};
+	}
 
 }
+
