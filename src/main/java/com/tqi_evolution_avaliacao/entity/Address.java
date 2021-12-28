62f31b1c959bfbd26e7a3413223aea5807aabcdd
Leandro Marcelino <leandro.souza.marcelino@gmail.com>
2021-12-27 22:29:07 -0300

    Criando a Entity do endereço

---
+++ src/main/java/com/tqi_evolution_avaliacao/entity/Address.java
@@ -0,0 +1,95 @@
+package com.tqi_evolution_avaliacao.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_Address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn (name = "id_client")
+    private Client client;
+    private String rua;
+    private int numero;
+    private String logadouro;
+    private String bairro;
+    private String cidade;
+    @Column(unique = true)
+    private String cep;
+    private String estado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getLogadouro() {
+        return logadouro;
+    }
+
+    public void setLogadouro(String logadouro) {
+        this.logadouro = logadouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
+
