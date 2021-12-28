53bb20f0c618e6aae822839c18c6856caee9f8b0
Leandro Marcelino <leandro.souza.marcelino@gmail.com>
2021-12-27 22:19:11 -0300

    Iniciando projeto, Criando a Entity do cliente

---
+++ src/main/java/com/tqi_evolution_avaliacao/entity/Client.java
@@ -0,0 +1,76 @@
+package com.tqi_evolution_avaliacao.entity;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "tb_clientes")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String cpf;
+    @Column(unique = true)
+    private String rg;
+    private Double renda;
+    @Column(nullable = false)
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public Double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(Double renda) {
+        this.renda = renda;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
